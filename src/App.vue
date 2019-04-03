b5c79849da17ad1d31e478dd1b47e7f6b5a7e471
neuronet.io <neuronet.io@gmail.com>
2019-04-04 00:19:11 +0200

    update 0.11.0

--- src/App.vue
+++ src/App.vue
@@ -9,121 +9,134 @@
 
 <script>
 import Vue from "vue";
-import Vuex from "vuex";
+//import Vuex from "vuex";
 import dayjs from "dayjs";
 import GanttElastic from "gantt-elastic";
 import GanttHeader from "gantt-elastic/src/components/Header.vue";
 
-Vue.use(Vuex);
-const store = new Vuex.Store({
-  state: {
-    tasks: [
+const tasks = [
+  {
+    id: 1,
+    label: "Make some noise",
+    user:
+      '<a href="https://www.google.com/search?q=John+Doe" target="_blank" style="color:#0077c0;">John Doe</a>',
+    start: new Date().getTime(),
+    duration: 15 * 24 * 60 * 60,
+    progress: 85,
+    type: "project",
+    collapsed: false
+  },
+  {
+    id: 2,
+    label: "With great power comes great responsibility",
+    user:
+      '<a href="https://www.google.com/search?q=Peter+Parker" target="_blank" style="color:#0077c0;">Peter Parker</a>',
+    parentId: 1,
+    start: new Date().getTime(),
+    duration: 4 * 24 * 60 * 60,
+    progress: 50,
+    type: "milestone",
+    collapsed: true,
+    dependentOn: [1],
+    style: {
+      base: {
+        fill: "#1EBC61",
+        stroke: "#0EAC51"
+      }
+    }
+  }
+];
+/*
+for (let i = 3; i < 100; i++) {
+  tasks.push({
+    id: i,
+    label: "test",
+    user: "Test",
+    parentId: 1,
+    start: new Date().getTime(),
+    duration: 4 * 24 * 60 * 60,
+    progress: 50,
+    type: "milestone",
+    collapsed: true,
+    dependentOn: [1],
+    style: {
+      base: {
+        fill: "#1EBC61",
+        stroke: "#0EAC51"
+      }
+    }
+  });
+}
+*/
+
+const options = {
+  maxRows: 100,
+  maxHeight: 300,
+  row: {
+    height: 24
+  },
+  calendar: {
+    hour: {
+      display: true
+    }
+  },
+  taskList: {
+    expander: {
+      straight: false
+    },
+    columns: [
       {
         id: 1,
-        label: "Make some noise",
-        user:
-          '<a href="https://www.google.com/search?q=John+Doe" target="_blank" style="color:#0077c0;">John Doe</a>',
-        start: new Date().getTime(),
-        duration: 15 * 24 * 60 * 60,
-        progress: 85,
-        type: "project",
-        collapsed: false
+        label: "ID",
+        value: "id",
+        width: 40
       },
       {
         id: 2,
-        label: "With great power comes great responsibility",
-        user:
-          '<a href="https://www.google.com/search?q=Peter+Parker" target="_blank" style="color:#0077c0;">Peter Parker</a>',
-        parentId: 1,
-        start: new Date().getTime(),
-        duration: 4 * 24 * 60 * 60,
-        progress: 50,
-        type: "milestone",
-        collapsed: true,
-        dependentOn: [1],
+        label: "Description",
+        value: "label",
+        width: 200,
+        expander: true,
+        html: true
+      },
+      {
+        id: 3,
+        label: "Assigned to",
+        value: "user",
+        width: 130,
+        html: true
+      },
+      {
+        id: 3,
+        label: "Start",
+        value: task => dayjs(task.start).format("YYYY-MM-DD"),
+        width: 78
+      },
+      {
+        id: 4,
+        label: "Type",
+        value: "type",
+        width: 68
+      },
+      {
+        id: 5,
+        label: "%",
+        value: "progress",
+        width: 35,
         style: {
-          base: {
-            fill: "#1EBC61",
-            stroke: "#0EAC51"
-          }
-          /*'tree-row-bar': {
-            fill: '#1EBC61',
-            stroke: '#0EAC51'
+          "task-list-header-label": {
+            "text-align": "center",
+            width: "100%"
           },
-          'tree-row-bar-polygon': {
-            stroke: '#0EAC51'
-          }*/
+          "task-list-item-value-container": {
+            "text-align": "center",
+            width: "100%"
+          }
         }
       }
-    ],
-    options: {
-      maxRows: 100,
-      maxHeight: 300,
-      row: {
-        height: 24
-      },
-      calendar: {
-        hour: {
-          display: true
-        }
-      },
-      taskList: {
-        expander: {
-          straight: false
-        },
-        columns: [
-          {
-            id: 1,
-            label: "ID",
-            value: "id",
-            width: 40
-          },
-          {
-            id: 2,
-            label: "Description",
-            value: "label",
-            width: 200,
-            expander: true,
-            html: true
-          },
-          {
-            id: 3,
-            label: "Assigned to",
-            value: "user",
-            width: 130,
-            html: true
-          },
-          {
-            id: 3,
-            label: "Start",
-            value: task => dayjs(task.start).format("YYYY-MM-DD"),
-            width: 78
-          },
-          {
-            id: 4,
-            label: "Type",
-            value: "type",
-            width: 68
-          },
-          {
-            id: 5,
-            label: "%",
-            value: "progress",
-            width: 35,
-            style: {
-              "task-list-header-label": {
-                "text-align": "center",
-                width: "100%"
-              },
-              "task-list-item-value-container": {
-                "text-align": "center",
-                width: "100%"
-              }
-            }
-          }
-        ]
-      }
-      /*locale: {
+    ]
+  }
+  /*locale: {
         name: "fr",
         Now: "Aujourd'hui",
         "X-Scale": "Zoom",
@@ -161,9 +174,16 @@ const store = new Vuex.Store({
           return `${n}${o}`;
         }
       }*/
-    }
+};
+
+/*Vue.use(Vuex);
+const store = new Vuex.Store({
+  state: {
+    tasks,
+    options
   }
 });
+*/
 
 export default {
   name: "app",
@@ -172,18 +192,24 @@ export default {
     GanttHeader,
     GanttFooter: { name: "gantt-footer", template: `<div>footer</div>` }
   },
-  store,
+  //store,
   data() {
-    return {};
+    return {
+      tasks,
+      options
+    };
   },
-  computed: {
+  created() {
+    console.log(this);
+  }
+  /*computed: {
     tasks() {
       return this.$store.state.tasks;
     },
     options() {
       return this.$store.state.options;
     }
-  }
+  }*/
 };
 </script>
 
